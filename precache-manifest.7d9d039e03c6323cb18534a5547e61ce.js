fb7aa17f8ab73f629fc771dd597a83818f65c72f
Nilabjo Sanyal <n.sanyal@hotmail.com>
2018-12-14 04:46:51 +0530

    Updates

--- precache-manifest.7d9d039e03c6323cb18534a5547e61ce.js
+++ precache-manifest.7d9d039e03c6323cb18534a5547e61ce.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/stocks/static/js/runtime~main.27b89963.js"
   },
   {
-    "revision": "48f9d7fc31aa7d006fb3",
-    "url": "/stocks/static/js/main.48f9d7fc.chunk.js"
+    "revision": "4241a5adcc6c741ed0ad",
+    "url": "/stocks/static/js/main.4241a5ad.chunk.js"
   },
   {
     "revision": "ef37eb848e73984f015c",
     "url": "/stocks/static/js/1.ef37eb84.chunk.js"
   },
   {
-    "revision": "48f9d7fc31aa7d006fb3",
+    "revision": "4241a5adcc6c741ed0ad",
     "url": "/stocks/static/css/main.0b34d545.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/stocks/static/css/1.0e08339b.chunk.css"
   },
   {
-    "revision": "d4d5aefcc9978e96d5fffb2ea834c564",
+    "revision": "133f12f399afdd0e6ae18c6939696cc6",
     "url": "/stocks/index.html"
   }
 ];
