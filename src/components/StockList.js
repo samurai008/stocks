4edf400298acc81b32f50e9965a83688e9859d7f
Sanyal <nilabjo.sanyal@accenture.com>
2018-12-14 15:39:39 +0530

    Modal created

---
+++ src/components/StockList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class StockList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
